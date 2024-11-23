ab5edbc6e5993165cd200bc54d892062cf334704
Florencia Alcoba <134442653+falcoba98@users.noreply.github.com>
2024-11-23 15:50:46 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,14 @@
 // Funciones previas para la obtención de datos y la autenticación
+const CATEGORIES_URL = '/data/cats/cat.json';
+const PUBLISH_PRODUCT_URL = "/data/sell/publish.json";
+const PRODUCTS_URL = "/data/cats_products/";
+const PRODUCT_INFO_URL = "/data/products/";
+const PRODUCT_INFO_COMMENTS_URL = "/data/products_comments/";
+const CART_INFO_URL = "/data/user_cart/";
+const CART_BUY_URL = "/data/cart/buy.json";
+const EXT_TYPE = ".json";
+
+// Funciones previas para la obtención de datos y la autenticación
 const EXT_TYPE = ".json";
 
 let showSpinner = function() {

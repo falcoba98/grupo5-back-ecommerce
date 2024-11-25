a6a62d003aaaf0614dddc3523c999b95381101c1
Nadia <nadia.c0805@gmail.com>
2024-11-24 21:08:19 -0300

    Login y middleware fueron creados

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const cors = require('cors'); // Importar cors
+const jwt = require('jsonwebtoken');
+const secretKey = 'proyecto-backend';
 
 // Servir archivos desde la raíz actual
 //app.use(express.static(path.join(__dirname)));
@@ -14,38 +16,75 @@ app.use(express.json());
 app.use(cors());
 
 // Servir archivos estáticos
-app.use('/data', express.static('data'));
+//app.use('/data', express.static('data'));
+
+const authorizationMiddleware = (req,res,next)=>{
+    const {authorization} = req.headers
+    //verificamos que tenga el token de autorizacion.
+    if(!authorization){
+        return res.status(401).json({
+            error: 'No autorizado.',
+            message: 'El usuario no tiene permisos para acceder.'
+          });
+    }
+    //verificamos que el token sea valido.
+    jwt.verify(authorization, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                error: 'No autorizado.',
+                message: 'Token inválido o expirado.'
+              });
+        } else {
+            //Guardamos el token decodificado en una propiedad usuario.
+            req.user = decoded
+            //Continuamos a la api solicitada.
+            next()
+        }
+    });
+}
+
+app.post('/login', (req,res)=>{
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({message:"Email y contraseña son requeridos."})
+    }
+    const payload = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+    res.json({token})
+})
 
 // Rutas para los archivos JSON
-app.get('/data/cats/cat.json', (req, res) => {
+app.get('/data/cats/cat.json', authorizationMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, 'data', 'cats', 'cat.json'));
 });
 
-app.get('/data/sell/publish.json', (req, res) => {
+app.get('/data/sell/publish.json', authorizationMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, 'data', 'sell', 'publish.json'));
 });
 
-app.get('/data/cats_products/:file', (req, res) => {
+app.get('/data/cats_products/:file', authorizationMiddleware, (req, res) => {
     const fileName = req.params.file;
     res.sendFile(path.join(__dirname, 'data', 'cats_products', `${fileName}.json`));
 });
 
-app.get('/data/products/:file', (req, res) => {
+app.get('/data/products/:file', authorizationMiddleware, (req, res) => {
     const fileName = req.params.file;
     res.sendFile(path.join(__dirname, 'data', 'products', `${fileName}.json`));
 });
 
-app.get('/data/products_comments/:file', (req, res) => {
+app.get('/data/products_comments/:file', authorizationMiddleware, (req, res) => {
     const fileName = req.params.file;
     res.sendFile(path.join(__dirname, 'data', 'products_comments', `${fileName}.json`));
 });
 
-app.get('/data/user_cart/:file', (req, res) => {
+app.get('/data/user_cart/:file', authorizationMiddleware, (req, res) => {
     const fileName = req.params.file;
     res.sendFile(path.join(__dirname, 'data', 'user_cart', `${fileName}.json`));
 });
 
-app.get('/data/cart/buy.json', (req, res) => {
+app.get('/data/cart/buy.json', authorizationMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, 'data', 'cart', 'buy.json'));
 });
 
